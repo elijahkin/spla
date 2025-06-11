4647482ff11d8c616e8e958f75b63332ebc49de2
Elijah Kin <elijah.m.kin@gmail.com>
2025-06-10 20:52:43 -0700

    Applying Google formatting

--- src/tensor.h
+++ src/tensor.h
@@ -23,12 +23,14 @@ concept Arithmetic = requires(T a, T b) {
 
 // Implements a sparse tensor represented internally by a map `data_`. If an
 // index is not present as a key, its entry is implicitly `default_value_`.
-template <Arithmetic T, int64_t... shape> class Tensor {
-public:
+template <Arithmetic T, int64_t... shape>
+class Tensor {
+ public:
   typedef std::array<int64_t, sizeof...(shape)> Index;
 
   // This is required by the conversion constructor.
-  template <Arithmetic OtherT, int64_t... other_shape> friend class Tensor;
+  template <Arithmetic OtherT, int64_t... other_shape>
+  friend class Tensor;
 
   ///////////////////////
   // Factory Functions //
@@ -49,9 +51,9 @@ public:
   Tensor(T default_value) : default_value_(default_value) {}
 
   template <Arithmetic U>
-  Tensor(const Tensor<U, shape...> &other)
+  Tensor(const Tensor<U, shape...>& other)
       : default_value_(static_cast<T>(other.default_value_)) {
-    for (const auto &[key, val] : other.data_) {
+    for (const auto& [key, val] : other.data_) {
       data_[key] = static_cast<T>(val);
     }
   }
@@ -64,12 +66,12 @@ public:
   // by `operator[]`, which is necessary to ensure that default values are not
   // accidently written to `data_`.
   class SubscriptProxy {
-  public:
-    SubscriptProxy(Tensor<T, shape...> &vec, Index idx)
+   public:
+    SubscriptProxy(Tensor<T, shape...>& vec, Index idx)
         : vec_(vec), idx_(idx) {}
 
     // Implements the behavior for `vec[i] = val`.
-    SubscriptProxy &operator=(const T &val) {
+    SubscriptProxy& operator=(const T& val) {
       vec_.data_[idx_] = val;
       return *this;
     }
@@ -84,12 +86,13 @@ public:
       return vec_.default_value_;
     }
 
-  private:
-    Tensor<T, shape...> &vec_;
+   private:
+    Tensor<T, shape...>& vec_;
     Index idx_;
   };
 
-  template <typename... Args> SubscriptProxy operator[](Args... idx) {
+  template <typename... Args>
+  SubscriptProxy operator[](Args... idx) {
     return SubscriptProxy(*this, {static_cast<int64_t>(idx)...});
   }
 
@@ -97,8 +100,8 @@ public:
   // Miscellaneous //
   ///////////////////
 
-  friend std::ostream &operator<<(std::ostream &os,
-                                  const Tensor<T, shape...> &vec) {
+  friend std::ostream& operator<<(std::ostream& os,
+                                  const Tensor<T, shape...>& vec) {
     return os << std::format("{}", vec.data_);
   }
 
@@ -106,10 +109,10 @@ public:
 
   int64_t sparsity() const { return data_.size(); }
 
-  friend auto reduce(const Tensor<T, shape...> &vec,
+  friend auto reduce(const Tensor<T, shape...>& vec,
                      std::function<T(T, T)> op) {
     auto result = vec.default_value_ * (vec.elements_in() - vec.sparsity());
-    for (const auto &[_, val] : vec.data_) {
+    for (const auto& [_, val] : vec.data_) {
       result = op(result, val);
     }
     return result;
@@ -119,34 +122,34 @@ public:
   // Elementwise Operations //
   ////////////////////////////
 
-  auto &operator+=(const Tensor<T, shape...> &rhs) {
-    return this->apply_binary_inplace([](T &a, const T &b) { a += b; }, rhs);
+  auto& operator+=(const Tensor<T, shape...>& rhs) {
+    return this->apply_binary_inplace([](T& a, const T& b) { a += b; }, rhs);
   }
 
-  auto &operator-=(const Tensor<T, shape...> &rhs) {
-    return this->apply_binary_inplace([](T &a, const T &b) { a -= b; }, rhs);
+  auto& operator-=(const Tensor<T, shape...>& rhs) {
+    return this->apply_binary_inplace([](T& a, const T& b) { a -= b; }, rhs);
   }
 
-  auto &operator*=(const Tensor<T, shape...> &rhs) {
-    return this->apply_binary_inplace([](T &a, const T &b) { a *= b; }, rhs);
+  auto& operator*=(const Tensor<T, shape...>& rhs) {
+    return this->apply_binary_inplace([](T& a, const T& b) { a *= b; }, rhs);
   }
 
   template <typename UnaryOp>
-  friend auto apply_unary(UnaryOp op, const Tensor<T, shape...> &vec)
+  friend auto apply_unary(UnaryOp op, const Tensor<T, shape...>& vec)
       -> Tensor<decltype(op(std::declval<T>())), shape...> {
     auto result = full(op(vec.default_value_));
-    for (const auto &[key, val] : vec.data_) {
+    for (const auto& [key, val] : vec.data_) {
       result.data_[key] = op(val);
     }
     return result;
   }
 
   template <typename BinaryOp>
-  friend auto apply_binary(BinaryOp op, const Tensor<T, shape...> &lhs,
-                           const Tensor<T, shape...> &rhs)
+  friend auto apply_binary(BinaryOp op, const Tensor<T, shape...>& lhs,
+                           const Tensor<T, shape...>& rhs)
       -> Tensor<decltype(op(std::declval<T>(), std::declval<T>())), shape...> {
     auto result = full(op(lhs.default_value_, rhs.default_value_));
-    for (const auto &[key, lhs_val] : lhs.data_) {
+    for (const auto& [key, lhs_val] : lhs.data_) {
       if (auto it = rhs.data_.find(key); it != rhs.data_.end()) {
         // The key is in both lhs and rhs
         result.data_[key] = op(lhs_val, it->second);
@@ -155,7 +158,7 @@ public:
         result.data_[key] = op(lhs_val, rhs.default_value_);
       }
     }
-    for (const auto &[key, rhs_val] : rhs.data_) {
+    for (const auto& [key, rhs_val] : rhs.data_) {
       if (auto it = lhs.data_.find(key); it == lhs.data_.end()) {
         // The key is only in rhs
         result.data_[key] = op(lhs.default_value_, rhs_val);
@@ -164,24 +167,24 @@ public:
     return result;
   }
 
-  friend Tensor<T, shape...> pow(const Tensor<T, shape...> &lhs,
-                                 const Tensor<T, shape...> &rhs) {
+  friend Tensor<T, shape...> pow(const Tensor<T, shape...>& lhs,
+                                 const Tensor<T, shape...>& rhs) {
     return apply_binary([](T a, T b) { return std::pow(a, b); }, lhs, rhs);
   }
 
-private:
+ private:
   std::map<Index, T> data_;
   T default_value_;
 
-  auto &apply_binary_inplace(std::function<void(T &, const T &)> op,
-                             const Tensor<T, shape...> &rhs) {
-    for (auto &[key, lhs_val] : data_) {
+  auto& apply_binary_inplace(std::function<void(T&, const T&)> op,
+                             const Tensor<T, shape...>& rhs) {
+    for (auto& [key, lhs_val] : data_) {
       if (auto it = rhs.data_.find(key); it == rhs.data_.end()) {
         // The key is only in lhs
         op(lhs_val, rhs.default_value_);
       }
     }
-    for (const auto &[key, rhs_val] : rhs.data_) {
+    for (const auto& [key, rhs_val] : rhs.data_) {
       if (auto it = data_.find(key); it == data_.end()) {
         // The key is only in rhs
         data_[key] = default_value_;
@@ -198,38 +201,38 @@ private:
 ////////////////////////////
 
 template <Arithmetic T, int64_t... shape>
-auto abs(const Tensor<T, shape...> &vec) {
+auto abs(const Tensor<T, shape...>& vec) {
   return apply_unary([](T a) { return std::abs(a); }, vec);
 }
 
 template <Arithmetic T, int64_t... shape>
-auto exp(const Tensor<T, shape...> &vec) {
+auto exp(const Tensor<T, shape...>& vec) {
   return apply_unary([](T a) { return std::exp(a); }, vec);
 }
 
 template <Arithmetic T, int64_t... shape>
-auto operator+(const Tensor<T, shape...> &lhs, const Tensor<T, shape...> &rhs) {
+auto operator+(const Tensor<T, shape...>& lhs, const Tensor<T, shape...>& rhs) {
   return apply_binary([](T a, T b) { return a + b; }, lhs, rhs);
 }
 
 template <Arithmetic T, int64_t... shape>
-auto operator-(const Tensor<T, shape...> &lhs, const Tensor<T, shape...> &rhs) {
+auto operator-(const Tensor<T, shape...>& lhs, const Tensor<T, shape...>& rhs) {
   return apply_binary([](T a, T b) { return a - b; }, lhs, rhs);
 }
 
 template <Arithmetic T, int64_t... shape>
-auto operator*(const Tensor<T, shape...> &lhs, const Tensor<T, shape...> &rhs) {
+auto operator*(const Tensor<T, shape...>& lhs, const Tensor<T, shape...>& rhs) {
   return apply_binary([](T a, T b) { return a * b; }, lhs, rhs);
 }
 
 template <Arithmetic T, int64_t... shape>
-auto operator<(const Tensor<T, shape...> &lhs, const Tensor<T, shape...> &rhs) {
+auto operator<(const Tensor<T, shape...>& lhs, const Tensor<T, shape...>& rhs) {
   return apply_binary([](T a, T b) { return a < b; }, lhs, rhs);
 }
 
 template <Arithmetic T, int64_t... shape>
-auto operator==(const Tensor<T, shape...> &lhs,
-                const Tensor<T, shape...> &rhs) {
+auto operator==(const Tensor<T, shape...>& lhs,
+                const Tensor<T, shape...>& rhs) {
   return apply_binary([](T a, T b) { return a == b; }, lhs, rhs);
 }
 
@@ -237,23 +240,25 @@ auto operator==(const Tensor<T, shape...> &lhs,
 // Reduce Operations //
 ///////////////////////
 
-template <int64_t... shape> bool all(const Tensor<bool, shape...> &vec) {
+template <int64_t... shape>
+bool all(const Tensor<bool, shape...>& vec) {
   return reduce(vec, [](bool a, bool b) { return a && b; });
 }
 
-template <int64_t... shape> bool any(const Tensor<bool, shape...> &vec) {
+template <int64_t... shape>
+bool any(const Tensor<bool, shape...>& vec) {
   return reduce(vec, [](bool a, bool b) { return a || b; });
 }
 
 template <Arithmetic T, int64_t... shape>
-T dot(const Tensor<T, shape...> &lhs, const Tensor<T, shape...> &rhs) {
+T dot(const Tensor<T, shape...>& lhs, const Tensor<T, shape...>& rhs) {
   return reduce(lhs * rhs, [](T a, T b) { return a + b; });
 }
 
 template <Arithmetic T, int64_t... shape>
-double norm(const Tensor<T, shape...> &vec, int ord) {
+double norm(const Tensor<T, shape...>& vec, int ord) {
   return std::pow(reduce(pow(abs(vec), ord), [](T a, T b) { return a + b; }),
                   1.0 / ord);
 }
 
-} // namespace spla
+}  // namespace spla
--- src/test.h
+++ src/test.h
@@ -1,5 +1,3 @@
-#include "log.h"
-
 #include <chrono>
 #include <format>
 #include <iostream>
@@ -7,15 +5,19 @@
 #include <utility>
 #include <vector>
 
+#include "log.h"
+
 // This file is a simple testing framework for C++, taking heavy inspiration
 // from Google Test. We first define the `TestSuite` class itself, and then
 // several functions for making assertions within tests.
 
-#define TEST(name)                                                             \
-  void name();                                                                 \
-  static struct Register_##name {                                              \
-    Register_##name() { TestSuite::Register(#name, name); }                    \
-  } register_##name;                                                           \
+#define TEST(name)                      \
+  void name();                          \
+  static struct Register_##name {       \
+    Register_##name() {                 \
+      TestSuite::Register(#name, name); \
+    }                                   \
+  } register_##name;                    \
   void name()
 
 bool passed;
@@ -24,13 +26,13 @@ bool passed;
 // `RunAll` method executes all registered tests and prints their results to the
 // console, indicating whether each test passed or failed.
 class TestSuite {
-public:
-  static void Register(const std::string &name, void (*func)()) {
+ public:
+  static void Register(const std::string& name, void (*func)()) {
     tests_.push_back({name, func});
   }
 
   static void RunAll() {
-    for (const auto &[name, func] : tests_) {
+    for (const auto& [name, func] : tests_) {
       passed = true;
       auto begin = std::chrono::steady_clock::now();
       func();
@@ -46,19 +48,25 @@ public:
     }
   }
 
-private:
+ private:
   static inline std::vector<std::pair<std::string, void (*)()>> tests_;
 };
 
-inline void EXPECT_TRUE(bool condition) { passed &= condition; }
+inline void EXPECT_TRUE(bool condition) {
+  passed &= condition;
+}
 
-inline void EXPECT_FALSE(bool condition) { passed &= !condition; }
+inline void EXPECT_FALSE(bool condition) {
+  passed &= !condition;
+}
 
-template <typename T1, typename T2> inline void EXPECT_EQ(T1 val1, T2 val2) {
+template <typename T1, typename T2>
+inline void EXPECT_EQ(T1 val1, T2 val2) {
   EXPECT_TRUE(val1 == val2);
 }
 
-template <typename T1, typename T2> inline void EXPECT_NE(T1 val1, T2 val2) {
+template <typename T1, typename T2>
+inline void EXPECT_NE(T1 val1, T2 val2) {
   EXPECT_FALSE(val1 == val2);
 }
 
